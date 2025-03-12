2b4af3aed95025a1b2b763ca74c92d9d31cae557
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-03-12 11:11:09 +0100

    cleanup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,25 @@
 use anyhow::Result;
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use std::sync::Mutex;
+use std::io::{self, Read, Write};
+use std::net::{TcpListener, TcpStream};
 use std::sync::atomic::AtomicBool;
 use std::sync::mpsc::{self, RecvTimeoutError, SendError};
 use std::sync::{Arc, TryLockError, atomic};
+use std::sync::{Mutex, MutexGuard};
 use std::time::Duration;
+use std::{marker, thread};
 use tracing::debug;
 use uuid::Uuid;
 
 const IOBUF_SZ: usize = 256;
+const ACCEPT_INTERVAL: Duration = Duration::from_millis(50);
 const CH_RECV_YIELD: Duration = Duration::from_secs(1);
 const NET_RECV_YIELD: Duration = Duration::from_secs(1);
-const CH_BUFFER_SZ: usize = 10;
 
 type Chunk = Vec<u8>;
 
-struct ConfigEntry {
+struct TunnelConfig {
     id: Uuid,
     ingress_port: u16,
-    tunnel: Uuid,
 }
 
 struct Client {
@@ -29,7 +29,7 @@ struct Client {
     rx: Mutex<mpsc::Receiver<Chunk>>,
 }
 
-struct Task {
+struct Tunnel {
     id: Uuid,
     should_exit: AtomicBool,
     clients: Mutex<Vec<Arc<Client>>>,
@@ -38,18 +38,20 @@ struct Task {
     tunnel_rx: Mutex<mpsc::Receiver<(Uuid, Chunk)>>,
 }
 
-impl Task {
-    fn new(id: Uuid, tunnel_stream: TcpStream) -> Self {
+impl Tunnel {
+    fn new(id: Uuid, tunnel_stream: TcpStream) -> Arc<Self> {
         let (tunnel_tx, tunnel_rx) = mpsc::channel::<(Uuid, Chunk)>();
-
-        Self {
+        let tunnel = Arc::new(Self {
             id,
             should_exit: AtomicBool::new(false),
             clients: Mutex::new(Vec::new()),
             tunnel_stream,
             tunnel_tx: Mutex::new(tunnel_tx),
             tunnel_rx: Mutex::new(tunnel_rx),
-        }
+        });
+
+        spawn_tunnel_workers(&tunnel);
+        tunnel
     }
 
     fn new_client(&self, id: Uuid, stream: TcpStream) -> Arc<Client> {
@@ -69,6 +71,33 @@ impl Task {
     fn remove_client(&self, id: Uuid) {
         let mut clients = self.clients.lock().unwrap();
         clients.retain(|client| client.id != id);
+    }
+
+    fn drive_listener(self: &Arc<Self>, config: &TunnelConfig) {
+        let addr = format!("localhost:{}", config.ingress_port);
+        let listener = TcpListener::bind(&addr).unwrap();
+        listener.set_nonblocking(true).unwrap();
+
+        loop {
+            let (stream, addr) = match listener.accept() {
+                Ok((stream, addr)) => (stream, addr),
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock && self.signal_close_test() => {
+                    debug!("stopping due to exit signal");
+                    return;
+                }
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => {
+                    thread::sleep(ACCEPT_INTERVAL);
+                    continue;
+                }
+                Err(err) => panic!("{:?}", err),
+            };
+
+            debug!("accepted new client connection from {:?}", addr);
+
+            let id = Uuid::new_v4();
+            let client = self.new_client(id, stream);
+            spawn_client_workers(self, client);
+        }
     }
 
     // receives client data and sends it to the tunnel channel
@@ -91,13 +120,12 @@ impl Task {
                 }
                 Ok(n) => n,
                 Err(ref err)
-                    if err.kind() == std::io::ErrorKind::TimedOut
-                        && self.should_exit.load(atomic::Ordering::SeqCst) =>
+                    if err.kind() == io::ErrorKind::TimedOut && self.signal_close_test() =>
                 {
                     debug!("stopping due to exit signal");
                     return;
                 }
-                Err(ref err) if err.kind() == std::io::ErrorKind::TimedOut => continue,
+                Err(ref err) if err.kind() == io::ErrorKind::TimedOut => continue,
                 Err(err) => panic!("{:?}", err),
             };
 
@@ -114,21 +142,13 @@ impl Task {
 
     // sends tunnel channel messages to the upstream endpoint
     fn drive_tunnel_upstream(&self) {
-        let mut _tunnel_stream = &self.tunnel_stream;
-        let mut tunnel_rx = match self.tunnel_rx.try_lock() {
-            Ok(rx) => rx,
-            Err(TryLockError::WouldBlock) => panic!("couldn't acquire client rx lock"),
-            Err(err @ TryLockError::Poisoned(_)) => panic!("{:?}", err),
-        };
+        let mut tunnel_stream = &self.tunnel_stream;
+        let mut tunnel_rx = assert_exclusive(&self.tunnel_rx);
 
         loop {
-            match tunnel_rx.recv_timeout(CH_RECV_YIELD) {
-                Ok((client_id, chunk)) => {
-                    todo!("TODO: send tunnel message {:?} {:?}", client_id, chunk)
-                }
-                Err(RecvTimeoutError::Timeout)
-                    if self.should_exit.load(atomic::Ordering::SeqCst) =>
-                {
+            let (client_id, chunk) = match tunnel_rx.recv_timeout(CH_RECV_YIELD) {
+                Ok((client_id, chunk)) => (client_id, chunk),
+                Err(RecvTimeoutError::Timeout) if self.signal_close_test() => {
                     debug!("stopping due to exit signal");
                     close_rx_in_place(&mut tunnel_rx);
                     return;
@@ -138,6 +158,21 @@ impl Task {
                     debug!("stopping due to tunnel tx signal");
                     return;
                 }
+            };
+
+            let data = format!("{}: {:?}", client_id, chunk); // TODO: serialize message here
+            match tunnel_stream.write_all(data.as_bytes()) {
+                Ok(_) => (),
+                Err(err) => {
+                    debug!(
+                        "stopping because tunnel stream disconnected with err {:?}, doing shutdown",
+                        err
+                    );
+
+                    self.signal_close();
+                    close_rx_in_place(&mut tunnel_rx);
+                    return;
+                }
             }
         }
     }
@@ -145,19 +180,14 @@ impl Task {
     // receives client channel messages and sends them to the client
     fn drive_client_downstream(&self, client: &Client) -> Result<()> {
         let mut client_stream = &client.stream;
-        let mut client_rx = match client.rx.try_lock() {
-            Ok(rx) => rx,
-            Err(TryLockError::WouldBlock) => panic!("couldn't acquire client rx lock"),
-            Err(err @ TryLockError::Poisoned(_)) => panic!("{:?}", err),
-        };
+        let mut client_rx = assert_exclusive(&client.rx);
 
         loop {
             let chunk = match client_rx.recv_timeout(CH_RECV_YIELD) {
                 Ok(chunk) => chunk,
-                Err(RecvTimeoutError::Timeout)
-                    if self.should_exit.load(atomic::Ordering::SeqCst) =>
-                {
+                Err(RecvTimeoutError::Timeout) if self.signal_close_test() => {
                     debug!("stopping due to exit signal");
+                    self.remove_client(client.id);
                     close_rx_in_place(&mut client_rx);
                     return Ok(());
                 }
@@ -185,26 +215,24 @@ impl Task {
             let n = match tunnel_stream.read(&mut read_buf) {
                 Ok(0) => {
                     debug!("stopping because tunnel stream disconnected, doing shutdown");
-                    self.should_exit
-                        .store(true, std::sync::atomic::Ordering::SeqCst);
+                    self.signal_close();
                     let mut tunnel_tx = self.tunnel_tx.lock().unwrap();
                     close_tx_in_place(&mut tunnel_tx);
                     return;
                 }
                 Ok(n) => n,
                 Err(ref err)
-                    if err.kind() == std::io::ErrorKind::TimedOut
-                        && self.should_exit.load(atomic::Ordering::SeqCst) =>
+                    if err.kind() == io::ErrorKind::TimedOut && self.signal_close_test() =>
                 {
                     debug!("stopping due to exit signal");
                     return;
                 }
-                Err(ref err) if err.kind() == std::io::ErrorKind::TimedOut => continue,
+                Err(ref err) if err.kind() == io::ErrorKind::TimedOut => continue,
                 Err(err) => panic!("{:?}", err),
             };
 
             let chunk = read_buf[..n].to_vec();
-            let client_id: Uuid = Uuid::new_v4(); // TODO: parse message here
+            let client_id: Uuid = Uuid::new_v4(); // TODO: deserialize message here
             let client = self
                 .clients
                 .lock()
@@ -223,6 +251,23 @@ impl Task {
             }
         }
     }
+
+    fn signal_close_test(&self) -> bool {
+        self.should_exit.load(atomic::Ordering::SeqCst)
+    }
+
+    fn signal_close(&self) {
+        debug!("signaling tunnel close");
+        self.should_exit.store(true, atomic::Ordering::SeqCst);
+    }
+}
+
+fn assert_exclusive<T>(lock: &Mutex<T>) -> MutexGuard<'_, T> {
+    match lock.try_lock() {
+        Ok(rx) => rx,
+        Err(TryLockError::WouldBlock) => panic!("couldn't acquire client rx lock"),
+        Err(err @ TryLockError::Poisoned(_)) => panic!("{:?}", err),
+    }
 }
 
 fn close_tx_in_place<T>(tx: &mut mpsc::Sender<T>) {
@@ -235,37 +280,33 @@ fn close_rx_in_place<T>(rx: &mut mpsc::Receiver<T>) {
     *rx = sentinel_rx;
 }
 
-fn spawn_tunnel_workers(task: &Arc<Task>) {
+fn spawn_tunnel_workers(tunnel: &Arc<Tunnel>) {
     {
-        let task = Arc::clone(task);
-        std::thread::spawn(move || task.drive_tunnel_upstream());
+        let tunnel = Arc::clone(tunnel);
+        std::thread::spawn(move || tunnel.drive_tunnel_upstream());
     }
 
     {
-        let task = Arc::clone(task);
-        std::thread::spawn(move || task.drive_tunnel_downstream());
+        let tunnel = Arc::clone(tunnel);
+        std::thread::spawn(move || tunnel.drive_tunnel_downstream());
     }
 }
 
-fn spawn_client_workers(task: &Arc<Task>, client: &Arc<Client>) {
+fn spawn_client_workers(tunnel: &Arc<Tunnel>, client: Arc<Client>) {
     {
-        let task = Arc::clone(task);
-        let client = Arc::clone(client);
+        let tunnel = Arc::clone(tunnel);
+        let client = Arc::clone(&client);
 
-        std::thread::spawn(move || task.drive_client_upstream(&client));
+        std::thread::spawn(move || tunnel.drive_client_upstream(&client));
     }
 
-    {
-        let task = Arc::clone(task);
-        let client = Arc::clone(client);
-
-        std::thread::spawn(move || task.drive_client_downstream(&client));
-    }
+    let tunnel = Arc::clone(tunnel);
+    std::thread::spawn(move || tunnel.drive_client_downstream(&client));
 }
 
 struct State {
-    config: Vec<ConfigEntry>,
-    active: Vec<Task>,
+    waiting: Vec<Uuid>,
+    tunnels: Vec<Tunnel>,
 }
 
 fn main() {
